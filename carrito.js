76909ed1f69b3c71eb44a86ded47d8968bdff9ac
notsag01 <79101798+notsag01@users.noreply.github.com>
2021-11-12 17:23:44 -0300

    V3.2.3B

--- carrito.js
+++ carrito.js
@@ -5,9 +5,9 @@ import { carrito, ProductosCarrito } from "./main.js";
 
 $("#carrito").append(`
             <div class="container" id="carrito-toggle"> 
-                <div class="row">
-                    <div class="col-4">
-                        <img src="./assets/img/pulseras/acero/pulsera acero bolitas 210x210.jpg " style="max-width: 100%;" alt="">
+                
+                    <div>
+                        
                     </div>
                     <div id="nombre-producto-carrito" class="col d-flex justify-content-start">
                         
@@ -16,9 +16,15 @@ $("#carrito").append(`
             </div>                        
     `)
     
-    $("#carrito").click(()=>{
-        $("#carrito-toggle").toggle("slow");
-    })
+
+        $("#carrito").click(()=>{
+            $("#carrito-toggle").toggle("slow");
+        })
+    
+    
+
+
+    
 
    
         
