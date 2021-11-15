54349db6ac329a24add2268704847a4d7d73d59e
notsag01 <79101798+notsag01@users.noreply.github.com>
2021-11-15 15:05:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ class ProductosCarrito{
                             </ul>                           
                        
                     <br>
-              
+                
                 `
             )
         }
