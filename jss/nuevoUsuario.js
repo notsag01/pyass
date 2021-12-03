d0d12925821f514a6eb7c9ebff9bfc5798f7f339
notsag01 <gcordoba1981@gmail.com>
2021-12-03 12:14:52 -0300

    20211203B

--- jss/nuevoUsuario.js
+++ jss/nuevoUsuario.js
@@ -1,33 +1,39 @@
 
 class Ususario{
-    constructor(nombre, apellido, email){
+    constructor(usuario, nombre, apellido, email, password){
+        this.usuario=usuario
         this.nombre=nombre
         this.apellido=apellido
         this.email=email
+        this.password=password
 
     }
 }
 
 const usuarios=[]||JSON.parse(localStorage("usuarios"))
 
+const getUsuario=document.getElementById("usuario-nuevo-usuario")
 const getNombre=document.getElementById("nombre-nuevo-usuario")
 const getApellido=document.getElementById("apellido-nuevo-usuario")
 const getEmail=document.getElementById("email-nuevo-usuario")
+const getPassword=document.getElementById("password-nuevo-usuario")
 
 $("#nuevo-usuario").submit((e)=>{
     e.preventDefault()
 
+    const usuario =getUsuario.value
     const nombre =getNombre.value
     const apellido =getApellido.value
     const email =getEmail.value
+    const password =getPassword.value
 
     
 
-    const nuevoUsusario= new Ususario(nombre, apellido, email)
+    const nuevoUsusario= new Ususario(usuario,nombre, apellido, email, password)
     console.log(nuevoUsusario)
     addUsuario(nuevoUsusario)
 
-    if(nombre===""|| apellido==="" || email===""){
+    if(usuario===""||nombre===""|| apellido==="" || email===""||password===""){
         alert("Todos los campos deben ser completados")
     }else{
 
